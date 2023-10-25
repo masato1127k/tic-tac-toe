0c157057523722ba1e2cf966814423e4f4e3de8c
masato1127k <masa48691127@gmail.com>
2023-10-25 14:36:32 +0000

    TypeScriptを導入

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board({ xIsNext, squares, onPlay }) {
+  
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
